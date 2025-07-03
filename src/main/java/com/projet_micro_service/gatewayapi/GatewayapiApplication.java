3131bf0231c27bebe82ef8efd0a1d8276961e3d0
LouafiRazine <louafi.razine@gmail.com>
2025-07-03 15:17:24 +0200

    first commit

---
+++ src/main/java/com/projet_micro_service/gatewayapi/GatewayapiApplication.java
@@ -0,0 +1,13 @@
+package com.projet_micro_service.gatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayapiApplication.class, args);
+	}
+
+}
